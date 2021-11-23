2773fe11faad4b9d1db051acb628f8b87fb114d2
JRudransh <JRudransh@protonmail.com>
2021-11-23 12:15:24 +0530

    DAY 3

---
+++ day3.py
@@ -0,0 +1,51 @@
+# Arithmatic operators
+# + ##
+# - ##
+# * ##
+# / ##
+# %
+# **
+# //
+
+
+# Assignment operators
+# =    x = 1 ##
+# +=   x += 2 ##
+# -=   x -= 2 ##
+# *=   x *= 2
+# /=   x /= 2
+# %=   x %= 2
+# **=  x **= 2
+# //=  x //= 2
+
+
+# Relational operators
+# > 
+# >= ##
+# <
+# <= ##
+# == ##
+# !=
+
+# Logical operators
+# and
+# or
+# not
+
+# Bitwise operators
+# ~ complement operator
+# & and operator  
+# | or operator
+# ^ xor operator
+# << left shift
+# >> right shift
+
+# Membership operators
+# l = [1,3,4,6,8,9,2]
+# in  ## 2 in l
+# not in  ## 20 not in l
+
+
+# Identity operators
+# is
+# is not
