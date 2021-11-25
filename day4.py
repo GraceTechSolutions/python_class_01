401dc50a6a7ddc2f08ae27b155f10086e839baf4
JRudransh <JRudransh@protonmail.com>
2021-11-25 12:11:52 +0530

    Day 4

---
+++ day4.py
@@ -0,0 +1,69 @@
+# Module
+import math as m
+
+# Converts valut into next greater int
+# n = m.ceil(n)
+
+# Converts valut into next smaller int
+# n = m.floor(n)
+
+# Concert radians into degrees
+# n = m.degrees(n)
+
+# Concert degrees into radians
+# n = m.radians(n)
+
+# Converts value in sin
+# n = m.sin(n)
+
+# Converts value in cos
+# n = m.cos(n)
+
+# Converts value in tan
+# n = m.tan(n)
+
+# Return exponantion of x this is same as e **x
+# n = m.exp(n)
+
+# Return absolute value
+# n = m.fabs(n)
+
+# returns the factorial value accepts only positive integers
+# n = m.factorial(n)
+
+# Return the modulas value
+# n = m.fmod(n, o)
+
+
+# Return sum of the list
+# n = m.fsum(n)
+
+# Return float and intgrals part of n
+# n = m.modf(n)
+
+# Return the base 10 logarithm of x
+# n = m.log10(n)
+
+# Return the natural logarithm
+# n = m.log(base, n)
+
+# Return the positive suure root
+# n = m.sqrt(n)
+
+# Return the value of y power of x
+# n = m.pow(x, y)
+
+# Return the greatest common divisor of x and y
+# n = m.gcd(x, y)
+
+# Return True if n is a nan (not number), othewise returns False
+# n = m.isnan(n)
+
+# Return True if n is a infinity, othewise returns False
+# n = m.isinf(n)
+
+# Math module constants
+# m.pi
+# m.e
+# m.inf
+# m.nan
