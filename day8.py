9260fae7e31a845059e691f1a54fc8205ed3c5a0
JRudransh <JRudransh@protonmail.com>
2021-12-02 13:11:57 +0530

    day 8

---
+++ day8.py
@@ -0,0 +1,66 @@
+# str_ = '123456'
+
+#  0  1  2  3  4  5
+# '1  2  3  4  5  6'
+# -6 -5 -4 -3 -2 -1
+
+# Slicing syntex
+# string_name[start: end: step]
+#   Reverse
+#   print(str_[::-1])
+
+# Repeating
+# print(str_[0:2]*2)
+
+# str2 = '98765'
+# Concatination
+# res = str_ +' '+ str2
+# print(res)
+
+# Checking membership
+# text = 'I am Jay'
+# res = 'I' in text
+# print(res)
+
+# Comapring string
+# s1 = 'jay'
+# s2 = 'jay'
+# print(s1 is s2)
+
+# Removing empty space
+# rstrip()   # remove empty pace from right
+# lstrip()   # remove empty pace from left
+# strip()   # remove empty pace from both side
+#
+# text = 'Hello world     '
+# s1 = '     Hello world    '
+# print(text.rstrip() == s1.strip())
+
+# Finding sub string
+# text = '1234568912346123456123456123452345'
+# find(sub, start, end)
+    # text.find('4', 12)
+# index(sub, start, end)
+    # text.find('4', 12)
+
+# Counting
+# text = '1234568912346123456123456123452345'
+# count(sub, start, end)
+# res = text.count('4')
+# print(res)
+
+# Replacing
+# replace(main, replace)
+# text = 'Hello world'
+# res = text.replace('world', 'python')
+# print(te)
+
+# Splitting & joining
+# split(sep)
+# ''.join(list)
+# text = '1 2 3 4 5 6 7 8 9'
+# res = text.split()
+# print(res)
+# res = ''.join(res)
+# print(res)
+
