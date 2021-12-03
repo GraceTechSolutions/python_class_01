ea2575e20242dbfd5f94f13c5140e5fa644d26b7
JRudransh <JRudransh@protonmail.com>
2021-12-03 12:13:05 +0530

    Class

---
+++ day9.py
@@ -0,0 +1,52 @@
+# string = 'Hello python'
+
+# Uppercae
+# srt_upper = string.upper()
+# print(srt_upper)
+
+# Lowercase
+# srt_lower = string.lower()
+# print(srt_lower)
+
+# Title
+# srt_title = string.title()
+# print(srt_title)
+
+# Starts with
+# res = string.startswith('Hello')
+# print(res)
+
+# string = ' '
+
+# Character testing method
+#
+# isalnum()
+# This method returns True is all chars are alpha numaric (a-z, A-z, 0-9)
+#
+# isalpha()
+# This method returns True is all chars are alphabetic (a-z, A-z)
+# 
+# isdigit()
+# This method returns True is all chars are numaric (0-9)
+# 
+# isupper()
+# This method returns True is all chars are Uppercase
+# 
+# islower()
+# This method returns True is all chars are Lowercase
+# 
+# istitle()
+# This method returns True if each word of the string starts with uppercase
+# 
+# isspace()
+# This method returns True if string contains only space
+
+
+# Formatting
+# .format()
+# name = 'jay'
+# age = 25
+
+# greet = 'Hello, my name is {} and i am {}'.format(name, age)
+
+# print(greet)
