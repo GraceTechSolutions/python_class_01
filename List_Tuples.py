6877507321de400c0a2153fe108e1aa4f66ebf23
JRudransh <JRudransh@protonmail.com>
2021-12-13 10:25:54 +0530

    LIST CLASS

---
+++ List_Tuples.py
@@ -0,0 +1,33 @@
+l1 = ['Hello', 2.5, 199, 7j, False, 2.0]
+l2 = ['Data from list 2', 'Another Data from list 2']
+
+# Indexing & Slicing
+    # list1[2]
+    # list1[0:3] Same as list1[:3]
+    # list1[1:5:2] # Here 2 is step size
+
+
+# Create a list using range
+    #  Range generates a seq of int which can be stored in a list
+# r = range(0, 10)
+# my_list = list(r)
+
+# CRUD
+# create read update delete
+# l = [1,2,3,4]
+# l[n]
+
+# Update
+# list1
+# list1.append(element) # Append element at the end
+# list1[0] = 'Hii'
+
+# Delete
+# list1
+# del list1[0]
+# del list1[0:3]
+# del list1
+# list1.remove(2) # Delete first '2'
+
+# Other methods
+# list1.reverse()
